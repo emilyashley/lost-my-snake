b1c69d60a536ba7dc44ae2bac89337282bd6a253
Emily Ashley <ohemilyashley@gmail.com>
2016-04-05 08:09:10 +0000

    test run of form submission to pg

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
 var express = require('express');
 var app = express();
 var pg = require('pg');
+var bodyParser = require('body-parser');
 
 
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
+// Parse JSON (uniform resource locators)
+app.use(bodyParser.json());
+// Parse forms (signup/login)
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
 
 // views is directory for all template files
 app.set('views', __dirname + '/client/views');
@@ -15,22 +22,25 @@ app.get('/', function(request, response) {
   response.render('pages/index')
 });
 
-app.get('/test', function(request, response){
-  response.render('pages/test')
-});
 
-app.post('/test', function(request, response){
-  var name = request.body.name;
-  pg.connect(process.env.DATABASE_URL, function(err, client, done){
-    client.query('INSERT INTO test_table(name) VALUES ($1)', [name], function(err, result) {
-      done();
-      if (err)
-       { console.error(err); response.send("Error " + err); }
-      else
-       { response.redirect('/lost'); } //redirect to sumbissions
-    });
+//test form submission for single variable entry to db
+app.route('/test')
+  .get(function(request, response){
+    response.render('pages/test')
   })
-});
+  .post(function(request, response){
+    var name = request.body.name;
+    pg.connect(process.env.DATABASE_URL, function(err, client, done){
+      client.query('INSERT INTO test_table(name) VALUES ($1)', [name], function(err, result) {
+        done();
+        if (err)
+        { console.error(err); response.send("Error " + err); }
+        else
+        { response.redirect('/lost'); } //redirect to sumbissions
+      });
+    })
+  });
+
 
 
 // requests from database for snake, lost, and sighted tables
