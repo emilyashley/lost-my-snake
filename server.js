7bd83c10f207cd5485eb0fdbdd1c72b088503820
Emily Ashley <ohemilyashley@gmail.com>
2016-04-05 07:39:05 +0000

    intializing form to test db table

--- server.js
+++ server.js
@@ -15,7 +15,26 @@ app.get('/', function(request, response) {
   response.render('pages/index')
 });
 
-app.get('/db', function (request, response) {
+app.get('/test', function(request, response){
+  response.render('pages/test')
+});
+
+app.post('/test', function(request, response){
+  var name = request.body.name;
+  pg.connect(process.env.DATABASE_URL, function(err, client, done){
+    client.query('INSERT INTO test_table(name) VALUES ($1)', [name], function(err, result) {
+      done();
+      if (err)
+       { console.error(err); response.send("Error " + err); }
+      else
+       { response.redirect('/lost'); } //redirect to sumbissions
+    });
+  })
+});
+
+
+// requests from database for snake, lost, and sighted tables
+app.get('/snake', function (request, response) {
   pg.connect(process.env.DATABASE_URL, function(err, client, done) {
     if (err)
        { console.error(err); response.send("Error " + err); }
@@ -24,10 +43,42 @@ app.get('/db', function (request, response) {
       if (err)
        { console.error(err); response.send("Error " + err); }
       else
-       { response.render('pages/db', {results: result.rows} ); }
+       { response.render('pages/snake', {results: result.rows} ); }
     });
   });
 })
+
+
+app.get('/lost', function (request, response) {
+  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+    if (err)
+       { console.error(err); response.send("Error " + err); }
+    client.query('SELECT * FROM test_table', function(err, result) {
+      done();
+      if (err)
+       { console.error(err); response.send("Error " + err); }
+      else
+       { response.render('pages/lost', {results: result.rows} ); }
+    });
+  });
+})
+
+app.get('/sighted', function (request, response) {
+  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+    if (err)
+       { console.error(err); response.send("Error " + err); }
+    client.query('SELECT * FROM test_table', function(err, result) {
+      done();
+      if (err)
+       { console.error(err); response.send("Error " + err); }
+      else
+       { response.render('pages/sighted', {results: result.rows} ); }
+    });
+  });
+})
+
+
+
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
