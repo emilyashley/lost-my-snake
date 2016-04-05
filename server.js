8714b1ebc27022e84f9909d969b33a591c5157ad
Emily Ashley <ohemilyashley@gmail.com>
2016-04-05 07:39:05 +0000

    intializing form to test db table
    test run of form submission to pg
    
    initial leaflet
    
    fidget with leaflet settings, imported on bower & npm

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
 var express = require('express');
 var app = express();
 var pg = require('pg');
+var bodyParser = require('body-parser');
 
 
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
+// Parse JSON (uniform resource locators)
+app.use(bodyParser.json());
+// Parse forms (signup/login)
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
 
 // views is directory for all template files
 app.set('views', __dirname + '/client/views');
@@ -15,7 +22,29 @@ app.get('/', function(request, response) {
   response.render('pages/index')
 });
 
-app.get('/db', function (request, response) {
+
+//test form submission for single variable entry to db
+app.route('/test')
+  .get(function(request, response){
+    response.render('pages/test')
+  })
+  .post(function(request, response){
+    var name = request.body.name;
+    pg.connect(process.env.DATABASE_URL, function(err, client, done){
+      client.query('INSERT INTO test_table(name) VALUES ($1)', [name], function(err, result) {
+        done();
+        if (err)
+        { console.error(err); response.send("Error " + err); }
+        else
+        { response.redirect('/lost'); } //redirect to sumbissions
+      });
+    })
+  });
+
+
+
+// requests from database for snake, lost, and sighted tables
+app.get('/snake', function (request, response) {
   pg.connect(process.env.DATABASE_URL, function(err, client, done) {
     if (err)
        { console.error(err); response.send("Error " + err); }
@@ -24,10 +53,42 @@ app.get('/db', function (request, response) {
       if (err)
        { console.error(err); response.send("Error " + err); }
       else
-       { response.render('pages/db', {results: result.rows} ); }
+       { response.render('pages/snake', {results: result.rows} ); }
     });
   });
 })
+
+
+app.get('/lost', function (request, response) {
+  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+    if (err)
+       { console.error(err); response.send("Error " + err); }
+    client.query('SELECT * FROM test_table', function(err, result) {
+      done();
+      if (err)
+       { console.error(err); response.send("Error " + err); }
+      else
+       { response.render('pages/lost', {results: result.rows} ); }
+    });
+  });
+})
+
+app.get('/sighted', function (request, response) {
+  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+    if (err)
+       { console.error(err); response.send("Error " + err); }
+    client.query('SELECT * FROM test_table', function(err, result) {
+      done();
+      if (err)
+       { console.error(err); response.send("Error " + err); }
+      else
+       { response.render('pages/sighted', {results: result.rows} ); }
+    });
+  });
+})
+
+
+
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
